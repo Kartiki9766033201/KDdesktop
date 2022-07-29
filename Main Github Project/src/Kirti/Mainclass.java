530e2fbb926d141e43ab0b9b4f00e4528069f703
Kartiki9766033201 <kirti.darandale@gmail.com>
2022-07-29 15:21:01 +0530

    Main
    Hi Everyone,
    Kindly refer the master code

---
+++ Main Github Project/src/Kirti/Mainclass.java
@@ -0,0 +1,13 @@
+package Kirti;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Main Project ");
+
+	}
+
+}
